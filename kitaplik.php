199dfbba16e1dfa94e86af7a736f915ace97bc64
mehmetriza <m.riza95@gmail.com>
2014-11-09 23:21:55 +0200

    Update kitaplik.php

--- kitaplik.php
+++ kitaplik.php
@@ -1,25 +1,39 @@
-<form method="POST" action="index.php" >
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+<form method="POST" action="kutuphane.php" >
 <input type="text" name="kIsim" />
+<input type="hidden" name="islem" value="search" />
 <input type="submit"  />
 </form>
 <?php
+header('Content-Type: text/html; charset=utf-8');
 function apiTip($tip)
 {
 	switch($tip)
 	{
-		case"ara":
-			return "https://www.goodreads.com/search.xml?key=jWA4PG0LIZfFT1m3Xe2Gg&q=".$_POST["kIsim"]."";
+		case"search":
+			return "https://www.goodreads.com/search.xml?key=jWA4PG0LIZfFT1m3Xe2Gg&q=".urlencode($_REQUEST["kIsim"])."";
 		break;
-		case"goodReadsId":
-			return "https://www.goodreads.com/book/show/$_POST[kIsim]?format=xml&key=jWA4PG0LIZfFT1m3Xe2Gg";
+		case"bookName":
+			return "https://www.goodreads.com/book/show/".urlencode($_REQUEST["kIsim"])."?format=xml&key=jWA4PG0LIZfFT1m3Xe2Gg";
+		break;
+		case "katalogTara":
+			return "http://www.kutuphane.selcuk.edu.tr/katalog_tarama.aspx";
 		break;
 	}
 }
 function cUrll($tip=NULL)
 {
+	$gonderi["TextBox1"]=urlencode($_REQUEST["kIsim"]);
 	$ch = curl_init(apiTip($tip));
+	$state="/wEPDwULLTEyNzUwNzY1NTEPZBYCAgEPZBYCAgEPZBYCZg9kFgJmD2QWCAIJD2QWBGYPZBYCZg9kFgICBQ8PZBYCHglvbmtleWRvd24FowFpZihldmVudC53aGljaCB8fCBldmVudC5rZXlDb2RlKXtpZiAoKGV2ZW50LndoaWNoID09IDEzKSB8fCAoZXZlbnQua2V5Q29kZSA9PSAxMykpIHtkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgnQnV0dG9uMScpLmNsaWNrKCk7cmV0dXJuIGZhbHNlO319IGVsc2Uge3JldHVybiB0cnVlfTsgZAIBD2QWAmYPZBYCZg88KwALAGQCCw8WAh4HVmlzaWJsZWgWBGYPZBYCZg9kFgICAQ8QZGQWAWZkAgEPZBYCZg9kFgJmDzwrAAsAZAINDxYCHwFoFgICAQ9kFgJmD2QWAmYPPCsACwBkAg8PFgIfAWgWBGYPZBYCZg9kFgICAQ8QZGQWAWZkAgEPZBYCZg9kFgJmDzwrAAsAZGSPMw0XZS9Bj3MMKinuExAFNFcYp1Qkx65OuV/Sp3Zbmw==";
+	$validation="/wEWEgKQtI2GCwK7q7GGCALWlM+bAgKF2fXbAwKgwpPxDQKTg/7sCQLR5pjpDQKkxtWADwLqjefYBwKBia7QDgLw+urtCQLJ363zDQLAnIfOBgLH0LDtAgLUho6cCAL7y8ufCQLs0bLrBgKM54rGBrsrD05OUFjltuSuvx/UgzJP3E0Upk+pXHmdwFi6F2+z";
+	$search=$_REQUEST["kIsim"];
 	curl_setopt_array( $ch,array(
 	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_POSTFIELDS=>"__VIEWSTATE=".urlencode($state)."&__EVENTVALIDATION=".urlencode($validation)."&DropDownList1=anahtar&Dropdownlist2=hepsi&TextBox1=$search&Button1=Ara",
+	CURLOPT_POST=>true,
 	CURLOPT_AUTOREFERER => true,
 	CURLOPT_MAXREDIRS => 10,
 	CURLOPT_SSL_VERIFYPEER => false,
@@ -30,19 +44,48 @@ function cUrll($tip=NULL)
 	));
 	$content = curl_exec( $ch );
 	curl_close( $ch );
-	return new SimpleXMLElement($content);
+	return $content;
 }
-if($_POST)
+if($_REQUEST)
 { 
-	$xml=cUrll("ara");
-	echo "<table>";
-	foreach($xml->search->results->work as $hey)
+	
+	$xml=cUrll($_REQUEST["islem"]);
+	switch($_REQUEST["islem"])
 	{
-		echo "<tr>
-			<td align='center' ><img src='".$hey->best_book->image_url."' /></td>
-			<td> <a href='https://www.goodreads.com/book/show/".$hey->best_book->id."' >".$hey->best_book->title."</a></td>
-			</tr>";
+		case 'search':
+			$xml= new SimpleXMLElement($xml);
+			echo "<table>";
+				foreach($xml->search->results->work as $hey)
+				{
+					echo "<tr>
+						<td align='center' ><img src='".$hey->best_book->image_url."' /></td>
+						<td> <a href='kutuphane.php?islem=bookName&kIsim=".$hey->best_book->id."' >".$hey->best_book->title."</a></td>
+						</tr>";
+				}
+			echo "</table>";
+		break;
+		case 'bookName':
+			$xml= new SimpleXMLElement($xml);
+			echo "<table>";
+					echo "
+						<tr>
+							<td>kitap oyu 5 üzerinden ". $xml->book->average_rating ."</td>
+							<td>".$xml->book->title ."</td>
+							<td>".$xml->book->authors->author->name ."</td>
+							<td><img src='".$xml->book->authors->author->image_url ."' /></td>
+							<td>Kitap Sayfası ".$xml->book->num_pages ."</td>
+						</tr>
+						<tr>
+							<td align='center' ><img src='".$xml->book->image_url."' /></td>
+							<td> ".$xml->book->description ."</td>
+						</tr>";
+			echo "</table>";
+		break;
+		case 'katalogTara':
+			echo $xml;
+		break;
 	}
-	echo "</table>";
+	
+	
 }
 ?>
